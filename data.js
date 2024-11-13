e4620051d639262fb8e7fe1aeb52381a55190d3f
Eric Lee <lee.eric.03@gmail.com>
2024-11-12 20:21:15 -0700

    retrieves answers from custom question API

--- data.js
+++ data.js
@@ -1,61 +1,70 @@
 import Chart from "chart.js/auto";
 import {
-	getAnswerValue,
-	setAnswerValue,
+	getAllAnswerValues,
 	setAutoHeight
 } from "@formsort/custom-question-api";
 
-// Need a function that can transform: current weight, weight loss time horizon into a data object
-const allAnswers = getAnswerValue();
+// Need a function that can transform: current weight, goal weight, and weight loss time horizon into a fields for data.datasets.data
+async function getAllAnswers() {
+	const answers = await getAllAnswerValues();
+	const values = Object.values(answers);
+
+	console.log(`values: ${values}`);
+}
+getAllAnswers();
+
+const options = {
+	pointStyle: false,
+	fill: true,
+	backgroundColor: "rgb(229,229,229,0.8)",
+	borderColor: "#00000",
+	borderWidth: 3,
+	scales: {
+		x: {
+			// type: "time",
+			max: 2,
+			labels: ["January", "February", "March", "April", "May"],
+			// min: "February",
+			grid: {
+				display: false
+			},
+			ticks: {
+				align: "start",
+				crossAlign: "far",
+				count: 2
+			}
+		},
+		y: {
+			grid: {
+				display: false
+			},
+			ticks: {
+				count: 2,
+				crossAlign: "near"
+			}
+		}
+	},
+	plugins: {
+		legend: {
+			display: false
+		}
+	}
+};
+
+const data = {
+	datasets: [
+		{
+			data: [300, 300, 200]
+		}
+	]
+};
 
 (async function () {
 	const ctx = document.getElementById("data");
 
-	const data = [
-		{ month: "Jan", weight: 250 },
-		{ month: "Feb", weight: 250 },
-		{ month: "Jun", weight: 200 }
-	];
-
 	new Chart(ctx, {
 		type: "line",
-		options: {
-			pointStyle: false,
-			fill: true,
-			backgroundColor: "rgb(229,229,229,0.8)",
-			borderColor: "#00000",
-			borderWidth: 3,
-			scales: {
-				x: {
-					type: "category",
-					max: 2,
-					labels: ["January", "February", "March", "April", "May"],
-					// min: "February",
-					grid: {
-						display: false
-					}
-				},
-				y: {
-					grid: {
-						display: false
-					},
-					ticks: {
-						count: 2
-					}
-				}
-			},
-			plugins: {
-				legend: {
-					display: false
-				}
-			}
-		},
-		data: {
-			datasets: [
-				{
-					data: data.map((row) => row.weight)
-				}
-			]
-		}
+		data: data,
+		options: options
 	});
 })();
--- snowpack.config.js
+++ snowpack.config.js
@@ -1,10 +1,10 @@
-const fs = require("fs");
+// const fs = require("fs");
 
-const cert = fs.readFileSync("./localhost.cert");
-const key = fs.readFileSync("./localhost.key");
+// const cert = fs.readFileSync("./localhost.cert");
+// const key = fs.readFileSync("./localhost.key");
 
-module.exports = {
-	devOptions: {
-		secure: { cert, key }
-	}
-};
+// module.exports = {
+// 	devOptions: {
+// 		secure: { cert, key }
+// 	}
+// };
